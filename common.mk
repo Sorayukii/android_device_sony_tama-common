d19afa5129c1e350a7710c1b17211d98baddf91d
xenxynon <flynryder427@gmail.com>
2023-01-08 12:09:18 +0800

    tama-common-kddi: Drop mock hidl power stats
    * E android.hardware.power.stats@1.0-service-mock Failed to getEnergyData

--- common.mk
+++ common.mk
@@ -381,9 +381,8 @@ PRODUCT_PACKAGES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2.vendor \
-    android.hardware.power-service.sony-libperfmgr \
-    android.hardware.power.stats@1.0-service.mock
-
+    android.hardware.power-service.sony-libperfmgr
+    
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
