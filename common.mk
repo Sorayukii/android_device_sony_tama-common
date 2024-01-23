c35b4d31802a63ed297132085193744307cc4a67
ekkusa <dayesofficial@gmail.com>
2024-01-24 00:16:07 +0800

    tama-common-kddi: seccomp: Import atfwd@2.0.policy
    - Kang from PNX Sprout

--- common.mk
+++ common.mk
@@ -426,7 +426,8 @@ PRODUCT_PACKAGES += \
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
+    $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/seccomp/atfwd@2.0.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/atfwd@2.0.policy
 
 # Sensors
 PRODUCT_PACKAGES += \
