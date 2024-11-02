miyoubi/device_sony_tama

tama-common: Import CXD-224 NFC
* Required for NFC on Japanese devices.

common.mk
@@ -346,7 +346,6 @@ PRODUCT_PACKAGES += \
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.2-service \
     com.android.nfc_extras \
     NfcNci \
     libchrome.vendor \
@@ -355,6 +354,9 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/init.tama_kddi.nfc.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.tama_kddi.nfc.rc
 
 # Power
 PRODUCT_PACKAGES += \

Sorayukii/android_device_sony_tama-common

common.mk
@@ -327,7 +327,6 @@ PRODUCT_PACKAGES += \
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.2-service \
     com.android.nfc_extras \
     NfcNci \
     libchrome.vendor \
@@ -336,6 +335,9 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/init.tama.nfc.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.tama.nfc.rc
 
 # Power
 PRODUCT_PACKAGES += \