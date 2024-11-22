6d32a71168a502c4925f751cd6817fe365a0add6
KanonifyX <153967762+KanonifyX@users.noreply.github.com>
2024-11-22 22:57:36 +0000

    tama-common: configs: textclassifier: Add and ship prebuilt textclassifier
    *Helps fix the delay we have in copying text and that being available in clipboard
    
    Signed-off-by: Sorayukii <sorayukii69@gmail.com>

--- common.mk
+++ common.mk
@@ -409,6 +409,13 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     TetheringConfigOverlay
 
+# TextClassifier
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/textclassifier/actions_suggestions.universal.model:$(TARGET_COPY_OUT_SYSTEM)/etc/textclassifier/actions_suggestions.universal.model \
+    $(LOCAL_PATH)/configs/textclassifier/textclassifier.en.model:$(TARGET_COPY_OUT_SYSTEM)/etc/textclassifier/textclassifier.en.model \
+    $(LOCAL_PATH)/configs/textclassifier/lang_id.model:$(TARGET_COPY_OUT_SYSTEM)/etc/textclassifier/lang_id.model \
+    $(LOCAL_PATH)/configs/textclassifier/textclassifier.universal.model:$(TARGET_COPY_OUT_SYSTEM)/etc/textclassifier/textclassifier.universal.model
+
 # Trust HAL
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
