79c1126fe446a333f01662c9a4d8c316a5c9b919
LuK1337 <priv.luk@gmail.com>
2025-09-18 14:55:32 +0800

    tama-common: Migrate to AIDL LiveDisplay HAL
    Change-Id: I4e8be836e94d442b40c63a1ac7dade3c9d9c784c
    Signed-off-by: Sorayukii <sorayukii69@gmail.com>

--- common.mk
+++ common.mk
@@ -274,7 +274,8 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
+    vendor.lineage.livedisplay-service.sdm
+$(call soong_config_set,livedisplay_sdm,enable_dm,false)
 
 # Lights
 PRODUCT_PACKAGES += \
